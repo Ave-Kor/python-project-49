4b3e7cbf4d5f06f4d6e29a4506f05681a977ec52
Aleksei Kor <karmano@mail.ru>
2024-01-04 20:05:22 +0400

    upd brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,7 +3,6 @@ from brain_games.cli import welcome_user
 
 def main():
     print(f'Welcome to the Brain Games!')
-
-if __name__ == '__main__':
-    welcome_user()
+    if __name__ != '__main__':
+        welcome_user()
 
