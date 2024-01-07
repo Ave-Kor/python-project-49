9726d510c51f0327fe9f66776cbc58713c65307f
Aleksei Kor <karmano@mail.ru>
2024-01-07 13:40:27 +0400

    upd brain_calc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -8,14 +8,13 @@ def main():
     name = welcome_user()
 
     while count < 3:
-        random_number = randint(1, 100)
         print('What is the result of the expression?')
-        number1 = random_number
-        number2 = random_number
+        number1 = randint(1, 100)
+        number2 = randint(1, 100)
         operators = '+-*'
         operator = choice(operators)
 
-        def random_operator(number1, number2, operator):
+        def calculation(number1, number2, operator):
             if operator == '+':
                 return number1 + number2
             elif operator == '-':
@@ -23,13 +22,13 @@ def main():
             elif operator == '*':
                 return number1 * number2
         print(f'Question: {number1} {operator} {number2}')
-        rigth_answer = random_operator(number1, number2, operator)
+        answer = calculation(number1, number2, operator)
         user_input = int(input('Your answer: '))
-        if user_input == rigth_answer:
+        if user_input == answer:
             count += 1
             print('Correct!')
         else:
-            print(f'{user_input} is wrong answer ;(. Correct answer was {rigth_answer}.')
+            print(f'{user_input} is wrong answer ;(. Correct answer was {answer}.')
             print(f"Let's try again, {name}!")
             break
     if count == 3:
