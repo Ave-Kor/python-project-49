53f38bf236acb712bb67d69317c896817ce9b4c5
Aleksei Kor <karmano@mail.ru>
2024-01-09 13:51:48 +0400

    upd brain_games

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -3,32 +3,46 @@ from random import choice
 from brain_games.cli import welcome_user
 
 
+def calculation(number1, number2):
+    operators = '+-*'
+    operator = choice(operators)
+    if operator == '+':
+        return number1 + number2, operator
+    elif operator == '-':
+        return number1 - number2, operator
+    elif operator == '*':
+        return number1 * number2, operator
+
+
+def ask_question(number1, operator, number2):
+    print(f'Question: {number1} {operator} {number2}')
+    u_input = int(input('Your answer: '))
+    return u_input
+
+
+def generate_question():
+    number1 = randint(1, 100)
+    number2 = randint(1, 100)
+    answer, operator = calculation(number1, number2)
+    return answer, operator, number1, number2
+
+
+def check_answer(u_input, answer):
+    return u_input == answer
+
+
 def main():
     count = 0
     name = welcome_user()
-
     while count < 3:
         print('What is the result of the expression?')
-        number1 = randint(1, 100)
-        number2 = randint(1, 100)
-        operators = '+-*'
-        operator = choice(operators)
-
-        def calculation(number1, number2, operator):
-            if operator == '+':
-                return number1 + number2
-            elif operator == '-':
-                return number1 - number2
-            elif operator == '*':
-                return number1 * number2
-        print(f'Question: {number1} {operator} {number2}')
-        answer = calculation(number1, number2, operator)
-        user_input = int(input('Your answer: '))
-        if user_input == answer:
+        answer, operator, number1, number2 = generate_question()
+        u_input = ask_question(number1, operator, number2)
+        if check_answer(u_input, answer):
             count += 1
             print('Correct!')
         else:
-            print(f'{user_input} is wrong answer ;(. Correct answer was {answer}.')
+            print(f'{u_input} is wrong answer ;(. Correct answer was {answer}.')
             print(f"Let's try again, {name}!")
             break
     if count == 3:
