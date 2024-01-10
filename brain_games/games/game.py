cbad41fc1829188c7e55566c0d9b3d3e01d17785
Aleksei Kor <karmano@mail.ru>
2024-01-10 13:45:53 +0400

    upd game.py

--- brain_games/games/game.py
+++ brain_games/games/game.py
@@ -19,6 +19,22 @@ def ask_question(number1, operator, number2):
     return u_input
 
 
+def calculation_nod(number1, number2):
+    while number1 != 0 and number2 != 0:
+        if abc(number1) > number2:
+            number1 = number1 % number2
+        else:
+            number2 = number2 % number1
+    return number1 + number2
+
+
+
+def ask_nod(number1, number2):
+    print(f'Question: {number1} {number2}')
+    u_input = int(input('Your answer: '))
+    return u_input
+
+
 def generate_question():
     number1 = randint(1, 100)
     number2 = randint(1, 100)
